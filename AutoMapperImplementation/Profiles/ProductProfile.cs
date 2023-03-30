838ec7063a26de4bff70bd2cc4e7c6c85fab295f
Anil Gurau <gurau.anil@gmail.com>
2023-03-31 00:53:25 +0545

    Automapper registration profile

---
+++ AutoMapperImplementation/Profiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using AutoMapperImplementation.DTOs;
+using AutoMapperImplementation.Entities;
+
+namespace AutoMapperImplementation.Profiles
+{
+    public class ProductProfile: Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDTO>();
+        }
+    }
+}
